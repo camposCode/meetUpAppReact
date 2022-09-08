ad3ace6ae3d238e6188705bab250f5f4bc7bde36
camposCode <info@josecampos.io>
2022-09-08 14:04:12 +0200

    meetupitem and meetuplist done, meetups displayed

--- src/pages/AllMeetUp.jsx
+++ src/pages/AllMeetUp.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import MeetUpList from '../components/meetups/MeetUpList';
 
-const AllMeetUp = () => {
-    return (
-        <>
-            <h1>All Meetups page</h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam est impedit exercitationem error reiciendis eveniet odio deleniti. Culpa, sequi accusantium, impedit mollitia vitae velit, animi nobis eaque ducimus quisquam architecto?</p>
-        </>
-    )
-    }
+const allMeetUps = [
+  {
+    id: 'm1',
+    title: 'This is a first meetup',
+    image:
+      'https://images.unsplash.com/photo-1530735606451-8f5f13955328?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
+  {
+    id: 'm2',
+    title: 'This is a second meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
+];
 
-export default AllMeetUp
+function AllMeetupsPage() {
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetUpList meetups={ allMeetUps } />
+    </section>
+  );
+}
+
+export default AllMeetupsPage;
